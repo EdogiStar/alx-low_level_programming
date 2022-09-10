1184b84ffdb743f5bd2b9ebdbbb1c64c1edd3593
Isah Muhammad Alhaji <muqsitxx@gmail.com>
2022-09-10 10:18:18 -0700

    Corrected Task 1: Print statement based on output

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,23 +3,24 @@
 #include <stdio.h>
 /**
 * main - main block
-* Description: Assign a random number to the variable n each time it is executed
+* Description: Print statements based on the last digit of the random number.
 * Return: 0
 */
 int main(void)
 {
 int n;
+int m;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
+m = n % 10;
 
-printf("Last digit of %d ", n);
-printf(" is ");
-if (n > 5)
-printf("and is greater than 5 ");
-else if (n == 0)
-printf("and is 0");
+if (m > 5)
+printf("Last digit of %i is %i and is greater than 5\n", n, m);
+else if (m == 0)
+printf("Last digit of %i is %i and is 0\n", n, m);
 else
-printf("and is less than 6 and not 0");
+printf("Last digit of %i is %i and is less than 6 and not 0\n", n, m);
+
 return (0);
 }
